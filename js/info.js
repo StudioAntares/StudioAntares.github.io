e69f5feeecde0f9bc79b676cced8c67625c1fd23
Ivaylo Getov <contact@ivaylogetov.com>
2015-02-17 23:27:03 -0800

    updated about and contact

--- js/info.js
+++ js/info.js
@@ -1,5 +1,12 @@
 var aboutInfoText = 
-"<p>About Blah blah blah blah blah blah blah blah blah blah blah blah blah</p>"
+"<p>Studio Antares...</p>" +
+"<p>&nbsp;</p>" +
+"<p>is an association of projects and individuals.</p>" +
+"<p>works in the realm where art and environment are identical.</p>" +
+"<p>explores the social impact of art and environment through interaction among different human outlets: architecture, photography, film, design, etc..</p>" +
+"<p>fosters a creative atmosphere for individuals with varied professional backgrounds, specializations, and interests.</p>" +
+"<p>appoints a dedicated group of professionals to each project, who address the issues through a humanistic approach. </p>" +
+"<p>aims to inspire all parties involved in a project.</p>";
 
-var contactInfoText = 
-"<p>Contact Blah blah blah blah blah blah blah blah blah blah blah blah blah</p>"
+var contactInfoText = '<p>+1.310.390.6042</p>';
+var hide = '<c><n uers="znvygb:pbagnpg@fghqvb-nagnerf.pbz">pbagnpg@fghqvb-nagnerf.pbz</n></c>';
