3c796e12df8e0915328b6672f5fb82b0b9cd4506
Ivaylo Getov <ivaylo.getov@gmail.com>
2015-03-12 00:41:02 -0700

    final(?) round of changes

--- js/info.js
+++ js/info.js
@@ -2,7 +2,7 @@ var aboutInfoText =
 "<p>Studio Antares…</p>" +
 "<p>…works in the realm where art and environment become identical.</p>" +
 "<p>…fosters a creative atmosphere for individuals with varied skills, backgrounds, specializations, and interests.</p>" +
-"<p>…explores the social impact of art and environment through interaction among different human outlets: architecture, photography, film, etc…</p>" +
+"<p>…explores the social impact of art and environment through interaction among different human outlets: architecture, photography, film, and design.</p>" +
 "<p>…takes a hands-on approach to every project, finding unique solutions for each particular challenge.</p>" +
 "<p>…applies technology with a humanistic approach.</p>" +
 "<p>…aims to inspire all parties involved in a project.</p>";
